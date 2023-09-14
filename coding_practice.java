9b0c4464c87c8fcb6a2347c6a5ae0ae11e2bc70e
a1 <jennykim6890@naver.com>
2023-09-14 20:33:15 +0900

    진짜 이번엔 제대로 한다

---
+++ coding_practice.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import java.util.Scanner;
+
+public class coding_practice {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        System.out.println(a);
+    }
+}
